35f048464171def03e2890c07778223470a40e0e
Alright Chiu <scartissuee1991@gmail.com>
2015-12-04 22:38:19 +0800

    add folder inside content/

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,11 +36,11 @@ SITELOGO = u'http://alrightchiu.github.io/SecondRound/fig/antiwar.jpg'	# 也弄�
 
 
 
-MENUITEMS = [
-    ('Archives', '/archives.html'),
-    ('Categories', '/categories.html'),
-    ('Tags', '/tags.html'),
-]
+# MENUITEMS = [
+#     ('Archives', '/archives.html'),
+#     ('Categories', '/categories.html'),
+#     ('Tags', '/tags.html'),
+# ]
 
 # Blogroll
 # LINKS = (
