2b51c6de3636bc87321202dea89eab5c1c6f278b
Alright Chiu <scartissuee1991@gmail.com>
2015-12-04 09:01:13 +0800

    add theme Flux

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,6 +19,9 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+#Theme
+THEME = 'theme/Flux'
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
