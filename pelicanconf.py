6770fbfb8713406cf5f01ef7c58dfb169157c452
Alright Chiu <scartissuee1991@gmail.com>
2015-12-06 10:44:04 +0800

    pagination test

--- pelicanconf.py
+++ pelicanconf.py
@@ -72,7 +72,7 @@ SOCIAL = (
     ('github', '#'),
 )
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
