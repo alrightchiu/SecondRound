15c3b4fccf02cbde80982899b805e3c3f7d926b7
Alright Chiu <scartissuee1991@gmail.com>
2015-12-07 22:12:07 +0800

    Markdown article content

--- pelicanconf.py
+++ pelicanconf.py
@@ -72,7 +72,7 @@ SOCIAL = (
     ('github', '#'),
 )
 
-DEFAULT_PAGINATION = 5
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
